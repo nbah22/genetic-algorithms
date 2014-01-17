4bd80ade6993a7658abca8b9063585072024e335
Ivan Kornev <nbah22@gmail.com>
2014-01-17 15:01:55 +0400

    Added some description

--- Launch.py
+++ Launch.py
@@ -1,36 +1,18 @@
 import Knights
 import Genetic
-# import profile
-# import pstats
 import tkinter
 
-
+# You can change some parametres and see what happens
 params = {'random_parents': False,
           'mother_is_good': True,
           'father_is_good': True,
           'size': 25,
           'num_of_children': 150,
           'mutate_before_breeding': False,
-          'max_num_of_mutations': 3,
-          'max_num_of_old_mutations': 0,
+          'max_num_of_mutations': 5,  # The larger are fields - the more mutations you will need
+          'max_num_of_old_mutations': 0,  # Number of mutations of old generation
           'equal_individuals_are_allowed': False,
           'seed': None}
 
-#, seed='BVVVVqqqq1VVVaqqqtVVVWqqqrVVVVqqqq1VVVaqqqtVVVWqqqrVVVVqqqq1VVVaqqqtVVVWqqqrVVVVqqqq1VVVaqqqtVVVWqqqrVVVV')
-p = Knights.Population(x_size=5, y_size=5, **params)
-Genetic.GUI(p, columns=5, title='Knights')
-
-# def profile_cycle():
-# 	profile.run('p.cycle()', 'Cycle')
-# 	stats = pstats.Stats('Cycle')
-# 	stats.sort_stats('time')
-# 	stats.print_stats()
-
-# profile.run('p = Knights.Population(x_size=5, y_size=5, **params)', 'Population_init')
-# win = tkinter.Tk()
-# tkinter.Button(command=profile_cycle, text='Profile one cycle').pack()
-# stats = pstats.Stats('Population_init')
-# stats.strip_dirs()
-# stats.sort_stats('time')
-# stats.print_stats()
-# win.mainloop()
+p = Knights.Population(x_size=5, y_size=5, **params)  # You can try using x_size=8 and y_size=8 instead, then
+Genetic.GUI(p, columns=5, title='Knights')            # it is better to make max_num_of_mutations=7 or more
